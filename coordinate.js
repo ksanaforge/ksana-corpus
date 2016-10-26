8754c7de2feb21d4d486de8e3570e4cb0e422f90
yapcheahshen <yapcheahshen@gmail.com>
2016-10-26 14:59:29 +0800

    fix coordinate conversion error on article after first one

--- coordinate.js
+++ coordinate.js
@@ -5,11 +5,11 @@ const bsearch=require("./bsearch");
 const textutil=require("./textutil");
 //get the byte distance from kpos to begining of logical line
 //a logical line might span across multiple raw line
-const getUnicodeCharDis=function(kpos,loglineKpos,getRawLine){
-	var linedis=this.bookLineOf(kpos)-this.bookLineOf(loglineKpos);
+const getUnicodeCharDis=function(firstline,kpos,loglineKPos,getRawLine){	
+	var linedis=this.bookLineOf(kpos)-this.bookLineOf(loglineKPos);
 	var chardis=0;
 
-	var ln1=this.bookLineOf(loglineKpos);
+	var ln1=this.bookLineOf(loglineKPos)-firstline;
 	while (linedis) {
 		chardis+=getRawLine(ln1++).length;
 		linedis--;
@@ -23,13 +23,15 @@ const toLogicalPos=function(linebreaks,kpos,getRawLine,omitpunc) {
 		kpos=k.start;
 	}
 	const line=bsearch(linebreaks,kpos+1,true)-1;
-	const eoff  =this.charOf(linebreaks[line]);
-	const linedis=this.bookLineOf(kpos)-this.bookLineOf(linebreaks[line]);
-	const chardis=getUnicodeCharDis.call(this,kpos,linebreaks[line],getRawLine);
-	const l1=getRawLine(this.bookLineOf(kpos));
+	const loglineKPos=linebreaks[line];//kPos of logical line
+	const eoff  =this.charOf(loglineKPos);
+	const firstline=this.bookLineOf(linebreaks[0]);
+	const linedis=this.bookLineOf(kpos)-firstline;
+	const chardis=getUnicodeCharDis.call(this,firstline,kpos,loglineKPos,getRawLine);
+	const l1=getRawLine(this.bookLineOf(kpos)-firstline);
 	const ch=textutil.trimRight.call(this,l1,this.charOf(kpos),omitpunc).length;
 
-	const paragraphfirstline=getRawLine(this.bookLineOf(linebreaks[line]));
+	const paragraphfirstline=getRawLine(this.bookLineOf(loglineKPos)-firstline);
 	const prevcount=textutil.trimRight.call(this,paragraphfirstline,eoff,!omitpunc).length;
 	return {line,ch:ch+chardis-prevcount};
 }
@@ -39,9 +41,10 @@ const toLogicalRange=function(linebreaks,address,getRawLine){ //find logical lin
 	const end=toLogicalPos.call(this,linebreaks,krange.end,getRawLine);	
 	return {start,end};
 }
-const fromLogicalPos=function(textline,ch,startkpos,getRawLine){
-	var start=this.bookLineOf(startkpos);
-	var line=getRawLine(start);
+const fromLogicalPos=function(textline,ch,startkpos,firstline,getRawLine){
+	const start=this.bookLineOf(startkpos);
+	var line=getRawLine(start-firstline);
+	
 	var offset=textutil.trimRight.call(this,line,this.charOf(startkpos),true).length;
 	if ((line.length-offset)>=ch) { //ch is in this line
 		return startkpos+this.kcount(textline.substr(0,ch));
@@ -50,7 +53,8 @@ const fromLogicalPos=function(textline,ch,startkpos,getRawLine){
 	var now=start;
 	while (ch>line.length) { //decrease ch with length of raw line
 		ch-=line.length;
-		line=getRawLine(++now);
+		++now;
+		line=getRawLine(now-firstline);
 	}
 	t=line.substr(0,ch); //remain text from closest raw line till pos::ch
 	return textutil.advanceLineChar.call(this,startkpos,now-start,t);
