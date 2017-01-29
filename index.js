8bb6518f6d208bdc89a804d2a836a3afc1494996
yapcheahshen <yapcheahshen@gmail.com>
2017-01-29 21:11:54 +0800

    subtree in ksana-corpus-builder

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const Engine=require("./engine");
 const bsearch=require("./bsearch");
-const subtree=require("./subtree");
 const trimArticleField=require("./article").trimArticleField;
 const openCorpus=function(id,opts,readycb){
 	return Engine.open(id,opts,readycb);
 }
 const parseLink=require("./parselink");
 module.exports={openCorpus:openCorpus,bsearch:bsearch,parseLink:parseLink
-,trimArticleField:trimArticleField,subtree:subtree};
+,trimArticleField:trimArticleField};
